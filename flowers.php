9548daeb7d2ef0d3db68ef9dadb50fd7dac68a36
NgoToi <ngovantoi3112@gmail.com>
2024-11-28 20:21:33 +0700

    Code bai 1

---
+++ flowers.php
@@ -0,0 +1,25 @@
+<?php
+$flowers = [
+    [
+        "name" => "Hoa mai",
+        "description" => "Hoa mai vàng nở vào dịp Tết, biểu tượng của sự sung túc.",
+        "image" => "c:\Users\Acer\Documents\Zalo Received Files\BTTH01\BTTH\hoadep\doquyen.jpg"
+    ],
+    [
+        "name" => "Hoa đào",
+        "description" => "Hoa đào nở rực rỡ, mang đến không khí Tết ấm áp.",
+        "image" => "c:\Users\Acer\Documents\Zalo Received Files\BTTH01\BTTH\hoadep\haiduong.jpg"
+    ],
+    [
+        "name" => "Hoa đào",
+        "description" => "Hoa đào nở rực rỡ, mang đến không khí Tết ấm áp.",
+        "image" => "c:\Users\Acer\Documents\Zalo Received Files\BTTH01\BTTH\hoadep\mai.jpg"
+    ],
+    [
+        "name" => "Hoa đào",
+        "description" => "Hoa đào nở rực rỡ, mang đến không khí Tết ấm áp.",
+        "image" => "c:\Users\Acer\Documents\Zalo Received Files\BTTH01\BTTH\hoadep\tuongvy.jpg"
+    ],
+
+];
+?>
